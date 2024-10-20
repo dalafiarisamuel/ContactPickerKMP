e634ed2ba5a08bec0fa0257f1e57947a717b9d42
Dalafiari Samuel <dalafiarisamuel@gmail.com>
2024-10-20 11:15:50 +0100

    add maven publish

--- contactpicker/build.gradle.kts
+++ contactpicker/build.gradle.kts
@@ -1,3 +1,4 @@
+import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
@@ -6,6 +7,51 @@ plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsCompose)
     alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.vanniktechMavenPublish)
+}
+
+mavenPublishing {
+    // Define coordinates for the published artifact
+    coordinates(
+        groupId = "io.github.dalafiarisamuel",
+        artifactId = "contactpicker",
+        version = "0.1.0"
+    )
+
+    // Configure POM metadata for the published artifact
+    pom {
+        name.set("KMP Library to natively select a contact")
+        description.set("This library can be used by Android and iOS targets for the shared functionality of selecting a contact")
+        inceptionYear.set("2024")
+        url.set("https://github.com/dalafiarisamuel/ContactPickerKMP")
+
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://opensource.org/licenses/MIT")
+            }
+        }
+
+        // Specify developers information
+        developers {
+            developer {
+                id.set("devTamuno")
+                name.set("Samuel Dalafiari")
+                email.set("dalafiarisamuel@gmail.com")
+            }
+        }
+
+        // Specify SCM information
+        scm {
+            url.set("https://github.com/dalafiarisamuel/ContactPickerKMP")
+        }
+    }
+
+    // Configure publishing to Maven Central
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    // Enable GPG signing for all publications
+    signAllPublications()
 }
 
 kotlin {
@@ -14,6 +60,7 @@ kotlin {
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_11)
         }
+        publishLibraryVariants("release", "debug")
     }
 
     targets.configureEach {
